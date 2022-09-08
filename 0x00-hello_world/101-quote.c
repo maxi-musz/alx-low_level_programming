6f61b83305ecf14a010976ffc65570a542a6caa5
oragon-hub <omayowagold@gmail.com>
2022-09-08 04:35:44 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	
 	return (1);
 }
