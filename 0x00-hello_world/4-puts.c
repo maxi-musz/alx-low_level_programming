6b5d9e06d33699527030cdf720d6f8127de6a4d2
oragon-hub <omayowagold@gmail.com>
2022-09-08 03:45:06 -0700

    Basic C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	puts("\"programming is like building a multilingual puzzle");
+	return(0)
+
+}
