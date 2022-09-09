da52ea5381f9171284cc55761fc3e41bd00b68a3
Oragon <90454403+oragon-hub@users.noreply.github.com>
2022-09-09 00:33:19 -0700

    Updated quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
 	return (1);
 }
