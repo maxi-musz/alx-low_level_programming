d2b5f9a77ab270b02744ae484cd329bc820dbfff
Oragon <90454403+oragon-hub@users.noreply.github.com>
2022-09-09 08:44:46 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
