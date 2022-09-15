c856d28482d385da25685bba460c8623c6656c1a
Oragon <90454403+oragon-hub@users.noreply.github.com>
2022-09-15 15:09:28 -0700

    edited main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-void print_most_numbers(void);
 void more_numbers(void);
 void print_line (int n);
 void print_diagonal(int n);
