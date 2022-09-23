f75bca66c89b08f2e1740ff2eb82822ede252918
oragon-hub <omayowagold@gmail.com>
2022-09-22 18:22:58 -0700

    added advanced tasks

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,7 +11,7 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember;
-	 * - you are not allowed to use a 
+	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anthing else than this line of code
