f3af13ed387f703c794d3f186644b8cc054b7577
oragon-hub <omayowagold@gmail.com>
2022-10-13 04:25:25 -0700

    Added script

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -13,6 +13,6 @@ int main(void)
     sum = sum_them_all(2, 98, 1024);
     printf("%d\n", sum);
     sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
+    printf("%d\n", sum);
     return (0);
 }
